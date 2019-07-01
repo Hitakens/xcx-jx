57d84b07247a66a6f15c161e2a6fe09cf5a83b38
zhangqjin <zhangqjinvip@163.com>
2019-07-02 00:02:43 +0800

    挖出是

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ App({
     },
   globalData: {
     webShowed: !1,
-    pathUrl:"http://192.170.1.104:8081/main",
+    pathUrl:"http://192.168.2.121:8081/main",
     imgUrl: "https://www.jaiy.online/",
     //pathUrl: "https://www.jaiy.online/spring-boot-qjin"
     }
